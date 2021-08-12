55c841e748486e1064bb0f66f3773ea18c1f8062
Oliver Scheer <o.t.scheer@gmail.com>
2021-08-12 13:46:06 +0200

    init

---
+++ ProJokes SwiftUI/ProJokes_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProJokes_SwiftUIApp.swift
+//  ProJokes SwiftUI
+//
+//  Created by Work on 12.08.21.
+//
+
+import SwiftUI
+
+@main
+struct ProJokes_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
